b670e95d592d07c6bafc629057c65ec14f76446f
Mathis <lb.brangeon@gmail.com>
2024-04-23 16:33:10 +0200

    ✨Add weather data cubit repositories

---
+++ lib/constante.dart
@@ -0,0 +1,1 @@
+const apiWeatherKey = '74f5546852d203a0b8b0082b4045e040';
---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:weather_app/constante.dart';
+import 'package:weather_app/models/weather.dart';
+
+class WeatherRepository {
+  WeatherRepository();
+
+  Future<List<Weather>> fetchWeather(LatLng latLng) async {
+    final Response response = await get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/forecast?lat=${latLng.latitude}&lon=${latLng.longitude}&appid=$apiWeatherKey'));
+    if (response.statusCode == 200) {
+      final List<Weather> weathers = [];
+      final Map<String, dynamic> json = jsonDecode(response.body);
+      if (json.containsKey("list")) {
+        final List<dynamic> days = json['list'];
+        for (Map<String, dynamic> day in days) {
+          final Weather weather = Weather.fromJson(day);
+          weathers.add(weather);
+        }
+      }
+      return weathers;
+    } else {
+      return [];
+    }
+  }
+}
