dbd0a6c67709fd9ba30ac25feed6a2b9cc041693
Mathis <lb.brangeon@gmail.com>
2024-04-23 16:33:44 +0200

    🐛Fix data adress

--- lib/models/adress.dart
+++ lib/models/adress.dart
@@ -1,24 +1,20 @@
 import 'package:latlong2/latlong.dart';
 
 class Adress {
-  final String street;
   final String city;
   final LatLng latLng;
 
-  factory Adress.fromGeoJson(Map<String, dynamic> json) {
+  factory Adress.fromGeoJson(
+    Map<String, dynamic> json,
+  ) {
     final Map<String, dynamic> properties = json['properties'] ?? {};
-    final String street = properties['name'];
     final String city = properties['city'];
     final List<dynamic> coordinates = json['geometry']['coordinates'];
-    return Adress(
-        street: street,
-        city: city,
-        latLng: LatLng(coordinates[1], coordinates[0]));
+    return Adress(city: city, latLng: LatLng(coordinates[1], coordinates[0]));
   }
 
   toJson() {
     return {
-      'street': street,
       'city': city,
       'lat': latLng.latitude,
       'lng': latLng.longitude,
@@ -26,11 +22,11 @@ class Adress {
   }
 
   factory Adress.fromJson(Map<String, dynamic> json) {
-    return Adress(
-        street: json['street'],
-        city: json['city'],
-        latLng: LatLng(json['lat'], json['lng']));
+    return Adress(city: json['city'], latLng: LatLng(json['lat'], json['lng']));
   }
 
-  Adress({required this.street, required this.city, required this.latLng});
+  Adress({
+    required this.city,
+    required this.latLng,
+  });
 }
