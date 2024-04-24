f497bed039d86ac6a03a4ac5a4808432a42e31eb
sofian-bali <95469123+sofian-bali@users.noreply.github.com>
2024-04-24 09:21:57 +0200

    ✨ Ajouter les cartes de favoris, la mise en page de l'écran de favoris et de la page Home

--- lib/ui/screens/favory.dart
+++ lib/ui/screens/favory.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/ui/widget/bottom_bar.dart';
-
+import 'package:weather_app/ui/widget/card_favory.dart';
 
 class FavoryScreen extends StatelessWidget {
   const FavoryScreen({super.key});
@@ -11,11 +11,29 @@ class FavoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Weather App'),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: const Text('Favorite Screen'),
+      
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          FavoryCard(
+            location: 'Angers',
+            onRemove: () {
+
+            },
+          ),
+          FavoryCard(
+            location: 'Angers',
+            onRemove: () {
+
+            },
+            
+          ),
+       
+
+        ],
       ),
       bottomNavigationBar: const CustomBottomAppBar(),
+
     );
   }
 }
